07bdd0731ca4ae8de96830e2b1a90adb0138d748
Anon4142 <slang-sways.0p@icloud.com>
2025-01-30 21:25:53 -0500

    Add other experiments.

--- README.md
+++ README.md
@@ -16,20 +16,25 @@ This repository contains the experiments and implementation code for **ESPFormer
    pip install -r requirements.txt
 ## Repository Structure
 
-The repository is organized as follows:
+Datasets will need to be downloaded manually. The repository is organized as follows:
 
 - **`attentions/`**: Contains the various attention modules
+- **`experiments/`**: 
 
-- **`catsdogs/`**: Image classification experiment on cats and dogs  
-  - **`examples/`**: contains train/test datasets (will need to be downloaded manually)
-  - **`trainer_cats_and_dogs.py`**: Loads the `.npy` subset indices if the `use_subset` argument is `True`.  
-  - **`one_expe.py`**: Entry script to run a single experiment. 
+   - **`catsdogs/`**: 
+      - **`examples/`**: contains train/test datasets (will need to be downloaded manually)
+      - **`trainer_cats_and_dogs.py`**: Loads the `.npy` subset indices if the `use_subset` argument is `True`.  
+      - **`one_expe.py`**: Entry script to run a single experiment. 
 
-- **`mnist/`**: Image classification on MNIST
-  - Dataset will need to be downloaded manually
-  - **`one_expe_mnist.py`**: Entry script to run a single experiment. 
+   - **`mnist/`**: 
+      - Dataset will need to be downloaded manually
+      - **`one_expe_mnist.py`**: Entry script to run a single experiment. 
+   
+   - **`nlp-tutorial/`**: 
+   - **`set_transformer/`**: 
   
 ## Running the Experiments
+- Run each experiment from their respective directories.
 ### Cats and Dogs
 
 1. **Download the Dataset**  
@@ -39,7 +44,7 @@ The repository is organized as follows:
    Rename the downloaded dataset folder to `examples` and place it inside the **`catsdogs/`** folder. 
 
 3. **Run the Experiment**  
-    Use the following command to run the experiment with a subset of the data:
+    From **`catsdogs/`**, run the following command to run the experiment with a subset of the data:
     ```bash
     python one_expe.py --subset_perc <subset-percentage> --use_subset
     ```
@@ -48,3 +53,11 @@ The repository is organized as follows:
     ```bash
     python one_expe.py
     ```
+
+
+## Acknowledgements
+GitHub repositories:
+- [DiffTransformer](https://github.com/microsoft/unilm/tree/master/Diff-Transformer)
+- [Sinkformer](https://github.com/michaelsdr/sinkformers)
+- [NLP](https://github.com/lyeoni/nlp-tutorial/tree/master/text-classification-transformer)
+- [Translation](https://github.com/facebookresearch/fairseq/blob/main/examples/translation/README.md)
--- attentions/esp.py
+++ attentions/esp.py
@@ -130,3 +130,77 @@ class EspAttention(nn.Module):
         return self.to_out(out), attn
 
 
+#####
+#Attention with key padding mask
+class EspGraph(nn.Module):
+    def __init__(self, d_in, heads=8, tau=1e-3, interp=None, agg_mode="closed_form", temperature=10):
+        super(EspGraph, self).__init__()
+
+        self.agg_mode = agg_mode
+        self.softsort = SoftSort_p2(tau=tau)
+        self.interp = interp
+        self.aggregator = GammaAggregator(
+            mode=agg_mode, 
+            num_heads=heads, 
+            num_slices=d_in if agg_mode == "learnable_manual" else None, 
+            temperature=temperature
+        )
+            
+    def forward(self, X, Y, mask=None):
+        
+        B, H, N, L = X.shape
+
+        if mask is not None:
+            expanded_mask = mask.unsqueeze(1).unsqueeze(-1).expand(-1, H, -1, L)
+            fill_value = 1e+18
+            X = torch.where(expanded_mask, X, torch.full_like(X, fill_value))
+            Y = torch.where(expanded_mask, Y, torch.full_like(Y, fill_value))
+        
+        Pu = self.softsort(X)  
+        Pv = self.softsort(Y)  
+    
+        # Compute Gamma
+        if self.interp is None:
+            Gamma = Pu.transpose(-1, -2) @ Pv  # Shape: [B, H, L, N, N]
+        else:
+            interp_expanded = self.interp.unsqueeze(0).unsqueeze(0).repeat(B, X_line.shape[-1], 1, 1).to(X.device)
+            Pu = Pu.unsqueeze(-1) if Pu.shape[-1] == 1 else Pu
+            Pv = Pv.unsqueeze(-1) if Pv.shape[-1] == 1 else Pv
+            Gamma = Pu.transpose(-1, -2) @ interp_expanded @ Pv
+    
+        Gamma_hat = self.aggregator(Gamma, x=X, y=Y) if self.agg_mode=="closed_form" else self.aggregator(Gamma)
+        return Gamma_hat
+
+
+class EspGraphAttention(nn.Module):
+    def __init__(self, dim, heads=8, dim_head=64, dropout=0., interp=None, learnable=True, agg_mode="closed_form", temperature=10, qkv_bias=False):
+        super().__init__()
+        inner_dim = dim_head * heads
+        project_out = not (heads == 1 and dim_head == dim)
+
+        self.heads = heads
+        self.scale = dim_head ** -0.5
+        self.esp = EspGraph(d_in=dim_head, heads=heads, interp=interp, agg_mode=agg_mode, temperature=temperature)
+
+        self.to_qkv = nn.Linear(dim, inner_dim * 3, bias=qkv_bias)
+        self.to_out = nn.Sequential(
+            nn.Linear(inner_dim, dim),
+            nn.Dropout(dropout)
+        ) if project_out else nn.Identity()
+
+    def forward(self, x, mask=None):
+        qkv = self.to_qkv(x).chunk(3, dim=-1)
+        q, k, v = map(lambda t: rearrange(t, 'b n (h d) -> b h n d', h=self.heads), qkv)
+        if mask is not None:
+            if mask.dim() == 4:  # Case: (B, H, N, N)
+                reduced_mask = mask.any(dim=1).any(dim=2)  # Reduce over heads and query dimensions
+            elif mask.dim() == 3:  # Case: (B, N, N)
+                reduced_mask = mask.any(dim=1)  # Reduce over query dimensions
+            else:
+                raise ValueError(f"Unexpected mask shape: {mask.shape}. Expected 3D or 4D tensor.")
+        else:
+            reduced_mask = None
+        attn = self.esp(q * self.scale, k * self.scale, reduced_mask)
+        out = torch.matmul(attn, v)
+        out = rearrange(out, 'b h n d -> b n (h d)')
+        return self.to_out(out), attn
